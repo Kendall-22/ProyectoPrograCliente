9cd91ed0b65d12b03573e9148bee8480362f2c1d
Sean17bcr <seanbcr123@gmail.com>
2022-08-07 14:55:57 -0600

    Version 0.1 creacion del main

---
+++ src/main/java/com/sc403/proyectoprograclientegit/Inicio.java
@@ -0,0 +1,13 @@
+package com.sc403.proyectoprograclientegit;
+public class Inicio {
+    public static void main(String[] args) {
+        //Clase main para el proyecto
+        
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
