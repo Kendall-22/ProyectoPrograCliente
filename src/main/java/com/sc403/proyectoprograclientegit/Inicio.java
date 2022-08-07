b5e96b86ede8a68487db28b51d0a874930040df2
Kendall-22 <ken.araya.solis@gmail.com>
2022-08-07 15:03:29 -0600

    MichaeL

--- src/main/java/com/sc403/proyectoprograclientegit/Inicio.java
+++ src/main/java/com/sc403/proyectoprograclientegit/Inicio.java
@@ -1,13 +1,7 @@
 package com.sc403.proyectoprograclientegit;
 public class Inicio {
     public static void main(String[] args) {
-        //Clase main para el proyecto
-        
-        
-        
-        
-        
-        // TODO code application logic here
+        System.out.println("Micheal Zorra");
     }
     
 }
